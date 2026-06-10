acc985b7dd10839f588a2aefa944c277f276110e
Phumai Ruangchit <Phumai.Ru@cjexpress.co.th>
2026-06-10 10:44:22 +0700

    Add main function and Vertex struct with initialization and print statements

---
+++ struct/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
+	v3 = Vertex{}
+	p  = &Vertex{1, 2}
+)
+
+func main() {
+	// v := Vertex{1, 2}
+	// v.X = 4
+	// v.Y = 10
+
+	// p := &v
+	// p.X = 100
+
+	// fmt.Println(v)
+
+	fmt.Println(v1, v2, v3, p)
+}
